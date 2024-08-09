9c5df11dfa0828eadbfa8888d014e28bbde5c227
davidvilas <davilasanvilar@gmail.com>
2024-08-09 10:34:02 +0200

    Little fixes

--- api/user/user.go
+++ api/user/user.go
@@ -7,7 +7,6 @@ import (
 	"vocablo/utils"
 
 	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
 )
 
 func Create(c *gin.Context) {
@@ -29,24 +28,24 @@ func Create(c *gin.Context) {
 	c.JSON(res.Status, res.Body)
 }
 
-func Get(c *gin.Context) {
-	unparsedId, _ := c.Params.Get("id")
-	parsedId, err := uuid.Parse(unparsedId)
-	if err != nil {
-		res := utils.ErrorResponse(http.StatusBadRequest, utils.GetStringPointer(err.Error()), nil)
-		c.AbortWithStatusJSON(res.Status, res.Body)
-		return
-	}
-	svc := svc.Get()
-	result, err := svc.User.Get(c.Request.Context(), parsedId)
-	if err != nil {
-		res := utils.InternalError(err)
-		c.AbortWithStatusJSON(res.Status, res.Body)
-		return
-	}
-	res := utils.SuccessResponse(result)
-	c.JSON(res.Status, res.Body)
-}
+// func Get(c *gin.Context) {
+// 	unparsedId, _ := c.Params.Get("id")
+// 	parsedId, err := uuid.Parse(unparsedId)
+// 	if err != nil {
+// 		res := utils.ErrorResponse(http.StatusBadRequest, utils.GetStringPointer(err.Error()), nil)
+// 		c.AbortWithStatusJSON(res.Status, res.Body)
+// 		return
+// 	}
+// 	svc := svc.Get()
+// 	result, err := svc.User.Get(c.Request.Context(), parsedId)
+// 	if err != nil {
+// 		res := utils.InternalError(err)
+// 		c.AbortWithStatusJSON(res.Status, res.Body)
+// 		return
+// 	}
+// 	res := utils.SuccessResponse(result)
+// 	c.JSON(res.Status, res.Body)
+// }
 
 // func Search(c *gin.Context) {
 // 	var form user.SearchForm
@@ -67,21 +66,21 @@ func Get(c *gin.Context) {
 // 	c.JSON(res.Status, res.Body)
 // }
 
-func Delete(c *gin.Context) {
-	unparsedId, _ := c.Params.Get("id")
-	parsedId, err := uuid.Parse(unparsedId)
-	if err != nil {
-		res := utils.ErrorResponse(http.StatusBadRequest, utils.GetStringPointer(err.Error()), nil)
-		c.AbortWithStatusJSON(res.Status, res.Body)
-		return
-	}
-	svc := svc.Get()
-	err = svc.User.Delete(c.Request.Context(), parsedId)
-	if err != nil {
-		res := utils.InternalError(err)
-		c.AbortWithStatusJSON(res.Status, res.Body)
-		return
-	}
-	res := utils.SuccessResponse(nil)
-	c.JSON(res.Status, res.Body)
-}
+// func Delete(c *gin.Context) {
+// 	unparsedId, _ := c.Params.Get("id")
+// 	parsedId, err := uuid.Parse(unparsedId)
+// 	if err != nil {
+// 		res := utils.ErrorResponse(http.StatusBadRequest, utils.GetStringPointer(err.Error()), nil)
+// 		c.AbortWithStatusJSON(res.Status, res.Body)
+// 		return
+// 	}
+// 	svc := svc.Get()
+// 	err = svc.User.Delete(c.Request.Context(), parsedId)
+// 	if err != nil {
+// 		res := utils.InternalError(err)
+// 		c.AbortWithStatusJSON(res.Status, res.Body)
+// 		return
+// 	}
+// 	res := utils.SuccessResponse(nil)
+// 	c.JSON(res.Status, res.Body)
+// }
